f75b648a3fd2c56576b47c5c3e4a5aca761c0efe
Evgeniy Kuleshov <kulevgen32@gmail.com>
2019-06-03 17:20:17 +0600

    Move listing files code to function and move it to Viewer class Optimized import and add path to interpreter

--- PYCommander.py
+++ PYCommander.py
@@ -1,14 +1,15 @@
-from os import name as os_name, system, listdir, curdir, path
+#!/usr/bin/env python3
 
+import os
+
+class Viewer():
+    def list_files(self, working_directory,):
+        file_list = os.listdir(working_directory)
+        for file in file_list:
+            print(file)
+
+viewer = Viewer()
 while True:
-    print(f'\nCurrent directory : {path.abspath(curdir)}')
+    print(f'\nCurrent directory : {os.path.abspath(os.curdir)}')
     directory = input('Enter a directory for view > ')
-    system('cls' if os_name == 'nt' else 'clear')
-    try:
-        file_list = listdir(directory)
-    except FileNotFoundError:
-        print('Unknown directory!')
-        continue
-    print('.\n..')
-    for file in file_list:
-        print(file)
+    viewer.list_files(directory)
