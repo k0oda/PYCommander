4aa776a5ec0dc5483710663225cb09872765f4a2
Evgeniy Kuleshov <kulevgen32@gmail.com>
2019-06-03 18:27:21 +0600

    Add input_prefix and change_directory function

--- PYCommander.py
+++ PYCommander.py
@@ -2,14 +2,19 @@
 
 import os
 
+
 class Viewer():
     def list_files(self, working_directory,):
         file_list = os.listdir(working_directory)
         for file in file_list:
             print(file)
 
+    def change_directory(self, target_directory):
+        os.chdir(target_directory)
+
+
 viewer = Viewer()
 while True:
-    print(f'\nCurrent directory : {os.path.abspath(os.curdir)}')
-    directory = input('Enter a directory for view > ')
+    input_prefix = f'{os.path.abspath(os.curdir)} $> '
+    directory = input(input_prefix)
     viewer.list_files(directory)
